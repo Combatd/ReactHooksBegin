6ebb287cf8c3a9f9edd8b6b1e318b7c3ebf2c085
Combatd <combatdstar@gmail.com>
2023-01-08 15:38:58 -0800

    Define #App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello! This component is App.js, which his rendered from index.js, since in there is a div #root in the public folder.</h1>
     </div>
   );
 }
