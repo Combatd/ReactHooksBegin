6a2b66e206eae6fd829211233fbf0877983e53c0
Combatd <combatdstar@gmail.com>
2023-01-09 14:41:06 -0800

    Uplift state in Class Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,29 @@
-function Button(props) {
-  return (
-    <button onClick={sayHello}>{props.label}</button>
-  )
-}
+import React from "react";
 
-// Functions are not returning any JSX!
-function sayHello() {
-  console.log("Hello");
-}
+// function Button(props) {
+//   
+// }
+
+
+
+class Button extends React.Component {
+  constructor() {
+    super();
+    this.state = { 
+      counter: 0
+    }
+    // this.handlleButtonClick = this.handlleButtonClick.bind(this); // without arrow function, bind the context of this to instance of Button
+  }
+  // If not arrow function, must use arrow function for onClick
+  handlleButtonClick() {
+    this.setState({ counter: 1})
+  }
+
+  render() {
+    return (
+      <button onClick={() => this.handlleButtonClick}>{this.props.label}</button>
+    )
+  }
+ }
 
 export default Button;
