c13a31f3f7bc7f81b3e038a588fe29400744ee26
Combatd <combatdstar@gmail.com>
2023-01-09 13:20:20 -0800

    Declare class App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Hello! This component is App.js, which his rendered from index.js, since in there is a div #root in the public folder.</h1>
-    </div>
-  );
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <h1>Hello! This component is App.js, which his rendered from index.js, since in there is a div #root in the public folder.</h1>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <h1>Hello! This Class is in App.js, which his rendered from index.js, since in there is a div #root in the public folder.</h1>
+    );
+  }
 }
 
 export default App;
