cb63fa4fb64292e96d00aaa7b1c5ba6b1360590d
Combatd <combatdstar@gmail.com>
2023-01-09 13:38:13 -0800

    Define #sayHello event for button

--- src/App.js
+++ src/App.js
@@ -9,11 +9,30 @@ import './App.css';
 //   );
 // }
 
+function Header() {
+  return (
+    <h1>Click the button!</h1>
+  )
+}
+
+function Button() {
+  return (
+    <button onClick={sayHello}>Click me!</button>
+  )
+}
+// Functions are not returning any JSX!
+function sayHello() {
+  console.log("Hello");
+}
+
 class App extends React.Component {
 
   render() {
     return (
-      <h1>Hello! This Class is in App.js, which his rendered from index.js, since in there is a div #root in the public folder.</h1>
+      <>
+        <Header />
+        <Button />
+      </>
     );
   }
 }
