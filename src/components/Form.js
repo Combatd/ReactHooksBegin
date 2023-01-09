cfac7a961ffe7d9880c8db259e0aa71e0cabe668
Combatd <combatdstar@gmail.com>
2023-01-09 15:48:13 -0800

    Fix inputs on Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
 class Form extends React.Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      value: 'Write your name'
+    }
+  }
+
   handleFormSubmit = (event) => {
     event.preventDefault();
-    console.log("Hey!");
+    alert('The state was changed: '+ this.state.value)
+  }
+  // we binded context to instane of form using arrow function
+  handleInputChange = (event) => {
+    this.setState({
+      value: event.target.value
+    })
   }
 
   render() {
     return (
       <form onSubmit={this.handleFormSubmit}>
-        <input id="name" type="text" />
+        <input id="name" type="text" value={this.state.value} onChange={this.handleInputChange} />
         <input type="submit" />
       </form>
     );
