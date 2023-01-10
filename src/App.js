673130368e759c2172937a478a289109fa14ceb0
Combatd <combatdstar@gmail.com>
2023-01-09 16:25:55 -0800

    Refactor Counter component to functions

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import './App.css';
 
-import Button from './components/Button';
+// import Button from './components/Button';
 import Counter from './components/Counter';
-import Header from './components/Header';
-import Form from './components/Form';
+// import Header from './components/Header';
+// import Form from './components/Form';
 
-// const App = () => {
-//   return (
-//     <div className="App">
-//       <h1>Hello! This component is App.js, which his rendered from index.js, since in there is a div #root in the public folder.</h1>
-//     </div>
-//   );
-// }
-
-class App extends React.Component {
-
-  render() {
-    return (
-      <>
-        <Header text="You spent: "/>
-        <Counter />
-        <Button label="Hey!" />
-        <Form />
-      </>
-    );
-  }
+const App = () => {
+  return (
+    <div> 
+      <Counter />
+    </div>
+  );
 }
+
+// class App extends React.Component {
+
+//   render() {
+//     return (
+//       <>
+//         <Header text="You spent: "/>
+//         <Counter />
+//         <Button label="Hey!" />
+//         <Form />
+//       </>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,33 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-class Counter extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      counter: 0
-    };
-  }
-  // After rendering, we will attach a timer to the component
-  componentDidMount() {
-    // We are defining the timer attribute, this is not built into React
-    this.timer = setInterval(() => {
-      this.setState({
-        counter: this.state.counter + 1
+// class Counter extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       counter: 0
+//     };
+//   }
+//   // After rendering, we will attach a timer to the component
+//   componentDidMount() {
+//     // We are defining the timer attribute, this is not built into React
+//     this.timer = setInterval(() => {
+//       this.setState({
+//         counter: this.state.counter + 1
+//       });
+//     }, 1000);
+//   }
+//   // When not rendered on the DOM anymore, remove the timer
+//   componentWillUnmount() {
+//     clearInterval(this.timer);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <span>{this.state.counter}</span>
+//       </div>
+//     )
+//   }
+// }
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0); // array destructuring of attributes/functions
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCounter((counter) => {
+        return counter + 1; 
       });
     }, 1000);
-  }
-  // When not rendered on the DOM anymore, remove the timer
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
 
-  render() {
-    return (
-      <div>
-        <span>{this.state.counter}</span>
-      </div>
-    )
-  }
-}
+    // component unmounts
+    return () => {
+      clearInterval(timer);
+    }
+  }, [])
+
+  return(
+    <div>
+      <span>You spent {counter} seconds</span>
+    </div>
+  );
+};
 
 export default Counter;
